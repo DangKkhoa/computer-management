6ca3f9ef7ac72aed7c9c8e2122b5f54642033492
DangKkhoa <dangkkhoa10a8@gmail.com>
2024-04-15 22:48:13 +0700

    Update home page

--- app/middleware/auth.middleware.js
+++ app/middleware/auth.middleware.js
@@ -1,10 +1,8 @@
 module.exports = (req, res, next) => {
     const { customer } = req.query;
-    console.log(req.session.user);
+    // console.log(req.session.user);
     if(req.session.user || customer === 'true') {
-        // console.log("I am here");
         return next();
     }
-    // console.log("I am outside");
     return res.redirect('/auth/login');
 }
